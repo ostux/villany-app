d4becb4a625d1444a0696b13ed936e34399db72a
Andras Toth <andras@andrastoth.eu>
2026-09-22 19:57:01 +0200

    Confiure Github Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
+  base: "/villany-app/", // GitHub Pages repository name
   build: {
     // Disable performance hints to avoid web vitals errors
     reportCompressedSize: false,
